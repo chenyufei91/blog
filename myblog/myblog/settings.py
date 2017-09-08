4ce038d0a875e66ee49542f05fcaf5d5d308b6ec
chenyufei91 <418587175@qq.com>
2017-09-08 16:42:03 +0800

    blog

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -188,7 +188,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
